e9ab0fcaf96e8a58a7f3fa3a4fd8df83edb8b03e
Nicholas Murdock <nick.murdock@BERKELEY.EDU>
2021-12-01 15:48:44 -0600

    Initial data anaylsis steps from 12.1.2021. Includes initial tables for the graphs. Still needs to tabulate countries from the listed geographic regions.

--- dataanalysis.r
+++ dataanalysis.r
@@ -7,5 +7,57 @@ data <- read.csv("LAg_sys_review_data.csv")
 # Viewing the beginning of the data
 head(data)
 
-# this is a test to push
-# Yes, hello Joseph!
+# Load packages
+library(tidyr)
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+library(lubridate)
+library(broom)
+
+# Test Analyses 
+hist(data$year)
+data %>%
+  group_by(eval_field) %>%
+  count()
+
+count(data[which(data$eval_field == 'Evaluation')])
+
+table(data$eval_field)
+
+data %>% 
+  group_by(eval_field) %>% 
+  count(unknown:polynesia)
+
+data %>%
+  gather()
+  group_by(eval_field) %>%
+  summarise(no = n()) %>%
+  spread(eval_field, no)
+# END TEST ANALYSES
+
+# Tables
+## Journal
+journal.table <- table(data$journal)
+
+## Year
+year.table <- table(data$year)
+
+## Evaluation / Field Use
+eval.table <- table(data$eval_field)
+
+## Population Descriptions
+population.table <- table(data$pop_descript)
+
+## Cohort Descriptions
+cohort.table <- table(data$cohort_descript)
+
+## Sample Type
+sample.table <- table(data$sample_type)
+
+## Assay Manufacturer
+manufac.table <- data %>%
+                  group_by(eval_field) %>%
+                  count(assay_manufact)
+
+
