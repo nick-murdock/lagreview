71715dbfc08d96848aaa4ed94f0856b8b966d354
Nicholas Murdock <nick.murdock@BERKELEY.EDU>
2021-12-01 14:48:46 -0600

    Attempt 2 at adding 8 lines of code to read in .csv data.

--- dataanalysis.r
+++ dataanalysis.r
@@ -1,1 +1,8 @@
 # THIS FILE IS FOR ALL DATA ANALYSIS PORTIONS OF THE COVIDENCE REVIEW
+
+# Loading in the data
+
+data <- read.csv("LAg_sys_review_data.csv")
+
+# Viewing the beginning of the data
+head(data)
